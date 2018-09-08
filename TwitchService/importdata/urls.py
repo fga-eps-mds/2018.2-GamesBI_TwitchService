bb0cfe630255fa8b8b8d8d2b2202fa75ca0520ab
Felipe Osório <felipe.osorio72@outlook.com.br>
2018-09-08 11:39:49 -0300

    Revert "[ADD] fga-eps-mds/2018.2-GamesBI#80 Started Twitch Service import data"

--- TwitchService/importdata/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-from .views import TwitchView
-
-urlpatterns = [
-    path('get_stream_list/', TwitchView.as_view(), name="get_stream"),
-]
--- TwitchService/settings.py
+++ TwitchService/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TwitchService.API',
-    'TwitchService.importdata',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
