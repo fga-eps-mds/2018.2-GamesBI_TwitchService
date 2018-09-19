7929199f25eb26d05ee041225c3130b94d497efb
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2018-09-19 14:37:50 -0300

    [HOTFIX] Added make test

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -48,3 +48,7 @@ fixture:
 loaddata:
 	# load fixtures saved upon importdata models
 	sudo docker-compose run twitchweb python manage.py loaddata data.json
+
+test:
+	# run unit tests
+	sudo docker-compose run twitchweb python manage.py test
