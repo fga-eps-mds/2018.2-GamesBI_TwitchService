510fef87dc50f700275eaaa7b1e25b82af6262f0
André Eduardo <andre.eduardo11@hotmail.com>
2018-09-19 10:14:57 -0300

    [FIX] fga-eps-mds/2018.2-GamesBI#80 Fix error at endpoint

--- TwitchService/API/views.py
+++ TwitchService/API/views.py
@@ -9,5 +9,5 @@ from .serializers import StreamSerializer
 class GamesTwich(APIView):
     serializers_class = StreamSerializer
     def get(self, request,format=None):
-        serializers = self.serializers_class(Stream.all(),many=True)
+        serializers = self.serializers_class(Stream.objects.all(),many=True)
         return Response(serializers.data)
